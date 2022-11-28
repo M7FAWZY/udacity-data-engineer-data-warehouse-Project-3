b56f220571191f5811b9de5085ff60c55a48de88
M7FAWZY <116055747+M7FAWZY@users.noreply.github.com>
2022-11-28 11:31:06 +0200

    Add files via upload

--- create_tables.py
+++ create_tables.py
@@ -1,33 +1,73 @@
-import configparser
+# I appreciate your Review
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-#drop tables before recreate
+
+def create_database():
+    """
+    - Creates and connects to the sparkifydb
+    - Returns the connection and cursor to sparkifydb
+    """
+    
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    
+    # create sparkify database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+
+    # close connection to default database
+    conn.close()    
+    
+    # connect to sparkify database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+    
+    return cur, conn
+
 
 def drop_tables(cur, conn):
+    """
+    Drops each table using the queries in `drop_table_queries` list.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
-#create fresh tables
+
 def create_tables(cur, conn):
+    """
+    Creates each table using the queries in `create_table_queries` list. 
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    cur = conn.cursor()
-
+    """
+    - Drops (if exists) and Creates the sparkify database. 
+    
+    - Establishes connection with the sparkify database and gets
+    cursor to it.  
+    
+    - Drops all the tables.  
+    
+    - Creates all tables needed. 
+    
+    - Finally, closes the connection. 
+    """
+    cur, conn = create_database()
+    
     drop_tables(cur, conn)
+    print("Table is dropped")
     create_tables(cur, conn)
-
+    print("Table is created") 
     conn.close()
 
 
 if __name__ == "__main__":
     main()
+    print(" # I appreciate your Review ")
